93dbdec7b2ddbd8ea4e6b1e3f1d98acdb990aaf5
shahzod777 <xurramovshaxzod777@gmail.com>
2023-09-05 04:28:05 -0700

    update

---
+++ Algorithms/C_Sharp/ProblemSolving/Solutions/Easy/RomanToInt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Solutions.Easy
+{
+    public class RomanToInt
+    {
+    }
+}
---
+++ Algorithms/C_Sharp/ProblemSolving/Solutions/Easy/TwoSum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Solutions.Easy
+{
+    public class TwoSum
+    {
+        public int[] Solution(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            throw new InvalidOperationException("No two elements of an array can add up to the target!");
+        }
+
+        public void Run()
+        {
+            try
+            {
+                Console.Write("Enter numbers of an array: ");
+                int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                Console.Write("Enter target number: ");
+                int targetNumber = int.Parse(Console.ReadLine());
+                int[] result = Solution(numbers, targetNumber);
+                Console.WriteLine($"Result: [{string.Join(", ", result)}]");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
